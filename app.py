8728a407639ae30312ca8665137f4a6771307949
LarsVomMars <lars@kroenner.eu>
2019-09-17 17:49:00 +0200

    Added new type

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 # TODO: Dynamic URL; idk how
 URL = "https://app.dsbcontrol.de/data/c0bc0590-65dc-470e-bd27-d5cd5ad140bd/dedb7e2a-7747-4751-b390-f4d9d8ac71c9/dedb7e2a-7747-4751-b390-f4d9d8ac71c9.htm"
-SEARCH_CLASS = re.compile('.*?TGI12/1.*?', re.IGNORECASE)
+# SEARCH_CLASS = re.compile('.*?TGI12/1.*?', re.IGNORECASE)
+SEARCH_CLASS = re.compile('.*?')
 
 #req = requests.get(URL)
 #page = BeautifulSoup(req.text, 'html.parser')
@@ -22,7 +23,10 @@ for day in days:
     for row in rows:
         if SEARCH_CLASS.match(row.td.b.string):
             fields = row.find_all('td')[1:]
-            if (fields[3].string == 'Entfall'):
-                txt = f":{fields[5].string}" if '&nbsp;' not in fields[5].prettify(formatter='html') else ''
-                print(f"{fields[0].b.string} {fields[1].string} entfaellt{txt}")
-
+            sub_type = fields[3].string
+            sub_les = f"{fields[0].b.string} {fields[1].string}"
+            if sub_type == 'Entfall':
+                txt = f": {fields[5].string}" if '&nbsp;' not in fields[5].prettify(formatter='html') else ''
+                print(f"{sub_les} entfaellt{txt}")
+            elif sub_type == 'Verlegung':
+                print(f"{sub_les} verlegt von {fields[4].string} in Raum {fields[2].string}{txt}")
