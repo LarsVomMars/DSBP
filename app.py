b3c954f0fe0e2a9be3f643b993db566f80165694
LarsVomMars <lars@kroenner.eu>
2019-09-18 14:00:39 +0200

    Finished types

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import requests
 import re
+import os
 from bs4 import BeautifulSoup
+
+from dotenv import load_dotenv
+load_dotenv()
+
+"""
+link id = open-link<F3>
+"""
 
 # TODO: Dynamic URL; idk how
 URL = "https://app.dsbcontrol.de/data/c0bc0590-65dc-470e-bd27-d5cd5ad140bd/dedb7e2a-7747-4751-b390-f4d9d8ac71c9/dedb7e2a-7747-4751-b390-f4d9d8ac71c9.htm"
@@ -22,11 +30,16 @@ for day in days:
     rows = day.find_all('tr', class_='list')[2:]
     for row in rows:
         if SEARCH_CLASS.match(row.td.b.string):
-            fields = row.find_all('td')[1:]
-            sub_type = fields[3].string
-            sub_les = f"{fields[0].b.string} {fields[1].string}"
+            fields = row.find_all('td')
+            sub_type = fields[4].string
+            sub_les = f"{fields[0].string}: {fields[1].b.string} {fields[2].string}"
+            room = fields[2].string
+            txt = f": {fields[6].string}" if '&nbsp;' not in fields[6].prettify(formatter='html') else ''
             if sub_type == 'Entfall':
-                txt = f": {fields[5].string}" if '&nbsp;' not in fields[5].prettify(formatter='html') else ''
                 print(f"{sub_les} entfaellt{txt}")
             elif sub_type == 'Verlegung':
-                print(f"{sub_les} verlegt von {fields[4].string} in Raum {fields[2].string}{txt}")
+                print(f"{sub_les} verlegt von {fields[5].string} in Raum {room}{txt}")
+            elif sub_type == 'Unterricht geändert':
+                print(f"{sub_les} geändert in {room}{txt}")
+            elif sub_type == 'Vertretung':
+                print(f"{sub_les} vertretung in {room}{txt}")
